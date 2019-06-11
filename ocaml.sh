fc89572794b6f3ce99e2cb2dbcb9c5f07292949c
floz <floz_pub@hotmail.com>
2019-06-11 16:31:27 +0800

    redesign the path

--- ocaml.sh
+++ ocaml.sh
@@ -1,8 +1,13 @@
-!#/bin/bash
-ocamllex pdsl_lexer.mll
-ocamlyacc pdsl_parser.mly
+#!/bin/bash
+rm -rf bin/ 
+mkdir bin/
+rm -rf build/
+mkdir build/
+cd build/
+ocamllex ../ocaml/pdsl_lexer.mll -o pdsl_lexer.ml
+menhir ../ocaml/pdsl_parser.mly --base pdsl_parser
 ocamlc -c pdsl_parser.mli
 ocamlc -c pdsl_lexer.ml
 ocamlc -c pdsl_parser.ml
-ocamlc -c pdsl.ml
-ocamlc -o PDSL pdsl_lexer.cmo pdsl_parser.cmo pdsl.cmo
+ocamlc -c ../ocaml/pdsl.ml -o pdsl.cmo
+ocamlc -o ../bin/pdsl pdsl_lexer.cmo pdsl_parser.cmo pdsl.cmo
